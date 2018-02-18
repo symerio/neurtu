c887bfb3ad50db18ed14a25bb598922b7bdbd4a2
Roman Yurchak <rth.yurchak@gmail.com>
2018-02-19 00:25:03 +0100

    Fix Travis CI

--- neurtu/tests/test_base.py
+++ neurtu/tests/test_base.py
@@ -1,25 +1,39 @@
 from __future__ import division
 
+import sys
 from time import sleep
+import pytest
 from pytest import approx
 
-from neurtu import Benchmark, timeit, memit, delayed
+from neurtu import timeit, memit, delayed
 
 
 def test_timeit_overhead():
 
-    dt = 0.02
+    dt = 0.2
 
     res = timeit(delayed(sleep)(dt))
 
     # overhead should be less than 500 us
-    assert res['wall_time_mean'] == approx(dt, abs=500e-6)
-    assert res['wall_time_max'] == approx(dt, abs=500e-6)
-    assert res['wall_time_min'] == approx(dt, abs=500e-6)
 
-    # measurement error should be less than 5%
-    assert res['wall_time_std'] / res['wall_time_mean'] < 0.002
-    print(res)
+    if sys.platform == 'win32':
+        # precision of time.time on windows is 16 ms
+        timer_precision = 25e-3
+    elif sys.platform == 'darwin':
+        # for some reason on OS X time.sleep appears to be
+        # quite inaccurate
+        timer_precision = 80e-3
+    else:
+        timer_precision = 5e-3
+
+    assert res['wall_time_mean'] == approx(dt, abs=timer_precision)
+    assert res['wall_time_max'] == approx(dt, abs=timer_precision)
+    assert res['wall_time_min'] == approx(dt, abs=timer_precision)
+
+    if sys.platform in ['win32', 'darwin']:
+        assert res['wall_time_std'] / res['wall_time_mean'] < 0.05
+    else:
+        assert res['wall_time_std'] / res['wall_time_mean'] < 0.002
 
 
 def test_memit_overhead():
@@ -31,15 +45,15 @@ def test_memit_overhead():
 
 
 def test_memit_array_allocation():
-    import array
-    double_size = 8
+    np = pytest.importorskip('numpy')
 
-    N = 3000
+    N = 5000
+    double_size = np.ones(1).nbytes
 
     def allocate_array():
-        X = array.array('d')
-        X.extend(1.0 for _ in range(N**2))
+        X = np.ones((N, N))
         sleep(0.1)
+        X[:] += 1
 
     res = memit(delayed(allocate_array)())
     assert res['peak_memory_mean'] == approx(N**2*double_size / 1024**2,
